e06cf6e40d6dd6a843f983634164bcaf955a1b6d
Oghenero-Sigbenu <sigbenuoghenero@gmail.com>
2020-07-13 15:34:45 +0100

    initial comit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showHomePage() {
+    let home = document.getElementById("home");
+    let landing = document.getElementById("landing");
+    home.style.display = "block";
+    landing.style.display = "none";
+}
